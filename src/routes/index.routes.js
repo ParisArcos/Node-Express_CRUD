3f4f414e662ee0a9de6661474004af1d258b44ca
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-12-28 15:35:10 +0000

    feat-> CRUD completed

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,20 +2,72 @@ const Router = require("express").Router;
 const router = Router();
 const Task = require("../models/Task");
 
-router.get("/", async (req, res) => {
-  const tasks = await Task.find();
-  res.render("index", { tasks: tasks });
+//!CREATE
+router.post("/tasks/add", async (req, res) => {
+  try {
+    const task = Task(req.body);
+    await task.save();
+    res.redirect("/");
+  } catch (error) {
+    alert("Error");
+    console.log(error);
+  }
 });
+
+//!READ
+router.get("/", async (req, res) => {
+  const tasks = await Task.find().lean();
+  const formAction = "/tasks/add";
+  const btnText = "Add Task";
+  res.render("index", {
+    tasks: tasks,
+    formAction: formAction,
+    btnText: btnText,
+  });
+});
+
 router.get("/about", (req, res) => {
   res.render("about");
 });
-router.get("/edit", (req, res) => {
-  res.render("edit");
+
+//!UPDATE
+router.get("/edit/:id", async (req, res) => {
+  const task = await Task.findById(req.params.id).lean();
+  const formAction = `/edit/${task._id}`;
+  const btnText = "Update Task";
+  res.render("edit", {
+    task: task,
+    formAction: formAction,
+    btnText: btnText,
+  });
 });
-router.post("/tasks/add", async (req, res) => {
-  const task = Task(req.body);
+
+router.get("/toogleDone/:id", async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  task.done = !task.done;
   await task.save();
   res.redirect("/");
+});
+
+//? deberia hacerse mediante PUT
+router.post("/edit/:id", async (req, res) => {
+  try {
+    await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//!DELETE
+//? deberia hacerse mediante DELETE
+router.get("/delete/:id", async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
